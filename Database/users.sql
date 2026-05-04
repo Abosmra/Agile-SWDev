90f829d27e9b56020a23b93fc725682f7ef27ccf
Mohamed <mohamedahmed2005@icloud.com>
2026-05-04 16:49:04 +0300

    Add FirstName and LastName columns to Users table, use AUTOINCREMENT

--- Database/users.sql
+++ Database/users.sql
@@ -1,13 +1,15 @@
 CREATE TABLE Users (
-    UserID INT PRIMARY KEY,
-    Username VARCHAR(50),
-    Password VARCHAR(50),
+    UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
     Role VARCHAR(20) -- Student, Staff, Admin
 );
 
-INSERT INTO Users VALUES (1, 'mai@example.com', '123456', 'Student');
-INSERT INTO Users VALUES (2, 'admin@example.com', 'admin123', 'Admin');
-INSERT INTO Users VALUES (3, 'staff1@example.com', 'pass123', 'Staff');
+INSERT INTO Users (Username, Password, FirstName, LastName, Role) VALUES ('mai@example.com', '123456', 'Mai', 'User', 'Student');
+INSERT INTO Users (Username, Password, FirstName, LastName, Role) VALUES ('admin@example.com', 'admin123', 'Admin', 'User', 'Admin');
+INSERT INTO Users (Username, Password, FirstName, LastName, Role) VALUES ('staff1@example.com', 'pass123', 'Staff', 'Member', 'Staff');
 
 /*
 Note: From Mai this is for implementing Sprint 1
