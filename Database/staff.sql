72d169564ad592cc4cd7d8f1264bbc084f024493
Mai Hamed <maiabostate2005@gmail.com>
2026-05-04 19:55:05 +0300

    added new sql tables

--- Database/staff.sql
+++ Database/staff.sql
@@ -9,6 +9,3 @@ INSERT INTO Staff VALUES (1, 'Dr. John Doe', 'Computer Science', 'john.doe@insti
 INSERT INTO Staff VALUES (2, 'Jane Smith', 'Information Systems', 'jane.smith@institution.edu');
 INSERT INTO Staff VALUES (3, 'Mike Johnson', 'Software Engineering', 'mike.johnson@institution.edu');
 
-/*
-Note: From Mai this is for implementing Sprint 1
-*/
